75820e276979f93acca984f9311c9c46014b6f34
Borislav Stanimirov <b@ibob.bg>
2024-11-18 14:02:51 +0200

    feat: load plugins

--- main.cpp
+++ main.cpp
@@ -1,16 +1,37 @@
-#include <ac/local/PluginLoader.hpp>
-#include <ac/local/ModelFactory.hpp>
 #include <ac/local/Model.hpp>
 #include <ac/local/Instance.hpp>
+#include <ac/local/Lib.hpp>
+#include <ac/local/ModelLoaderRegistry.hpp>
 
 #include <ac/jalog/Instance.hpp>
 #include <ac/jalog/sinks/DefaultSink.hpp>
 
 #include <iostream>
 
+#ifdef HAVE_ACLP_OUT_DIR
+#include "aclp-out-dir.h"
+#endif
+
 int main() try {
     ac::jalog::Instance jl;
     jl.setup().add<ac::jalog::sinks::DefaultSink>();
+
+#ifdef HAVE_ACLP_OUT_DIR
+    ac::local::Lib::addPluginDir(ACLP_OUT_DIR);
+#endif
+
+    ac::local::Lib::loadAllPlugins();
+
+    auto model = ac::local::Lib::modelLoaderRegistry().createModel({
+        .inferenceType = "foo",
+        .name = "foo-synthetic"
+        }, {});
+
+    auto instance = model->createInstance("general", {});
+
+    auto opResult = instance->runOp("run", { {"input", {"The", "song", "goes:"}}, {"splice", false}});
+
+    std::cout << opResult << "\n";
 }
 catch (std::exception& e) {
     std::cerr << "exception: " << e.what() << "\n";
